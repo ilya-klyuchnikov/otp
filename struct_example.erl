39b7544fbbef03aba0849a6d9ad35a40f1ee5915
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2024-11-04 11:45:37 +0000

    more

--- struct_example.erl
+++ struct_example.erl
@@ -1,6 +1,6 @@
 -module(struct_example).
 
--export([main/0]).
+-export([main/0, main1/0]).
 
 % ./bin/erl -noshell -eval 'struct_example:main()'
 
@@ -25,4 +25,17 @@ main() ->
     erlang:display(io_lib_format:fwrite("~p_~p", [Test1, Test2])),
     erlang:display(io_lib_format:fwrite("~p", [Obj])),
     erlang:display({struct_prototype:module(Obj), struct_prototype:name(Obj)}),
+    X = term_to_binary(Obj),
+    erlang:display(X),
+    Y = binary_to_term(X),
+    erlang:display(Y),
+    erlang:display(is_struct(Y)),
+    erlang:display({struct_prototype:module(Y), struct_prototype:name(Y)}),
+    halt(0).
+
+main1() ->
+    SerializedStruct = <<131,104,3,104,5,110,5,0,176,18,22,80,1,119,6,102,105,101,108,100,49,97,1,119,6,102,105,101,108,100,50,97,2,97,1,97,2>>,
+    Struct = binary_to_term(SerializedStruct),
+    erlang:display(Struct),
+    erlang:display({struct_prototype:module(Struct), struct_prototype:name(Struct)}),
     halt(0).
