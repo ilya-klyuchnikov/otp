568623a47d6f8b715582fc6a8e396da5fcfa0ccc
Ilya Klyuchnikov <ilyaklyuchnikov@meta.com>
2025-08-20 13:37:39 +0100

    fix cerl export_types

--- lib/compiler/src/cerl.erl
+++ lib/compiler/src/cerl.erl
@@ -140,7 +140,7 @@ function `type/1`.
         ]).
 
 -export_type([c_alias/0, c_apply/0, c_binary/0, c_bitstr/0, c_call/0, c_case/0, c_clause/0, c_cons/0, c_fun/0,
-	      c_let/0, c_letrec/0, c_literal/0, c_map/0, c_map_pair/0,
+	      c_let/0, c_letrec/0, c_literal/0, c_map/0, c_map_pair/0, c_struct/0, c_struct_pair/0,
 	      c_module/0, c_opaque/0, c_primop/0, c_tuple/0,
 	      c_values/0, c_var/0, cerl/0, var_name/0, dtype/0, c_lct/0]).
 
